fec6b63113ce431dab5bc7f79397c940dc8a6799
Andrew Hickling <ajh.hickling@gmail.com>
2024-07-25 10:16:10 +0100

    Added clearInterval() onDestroy to avoid memory leak memory

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,13 +1,17 @@
 <p>{hour} : {minutes} : {seconds}</p>
 
 <script>
+import { onDestroy } from "svelte";
+
 var date = new Date();
-setInterval(() => {
+const interval = setInterval(() => {
     date = new Date();
 }, 1000);
 
 $: hour = date.getHours().toString().padStart(2, '0');
 $: minutes = date.getMinutes().toString().padStart(2, '0');
 $: seconds = date.getSeconds().toString().padStart(2, '0');
+
+onDestroy(() => clearInterval(interval))
 
 </script>
